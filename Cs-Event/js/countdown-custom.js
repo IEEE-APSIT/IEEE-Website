6f47bb47b6cfde8ce9d1fe04f2027210e5adeb26
nikita24383 <55633694+nikita24383@users.noreply.github.com>
2020-05-20 00:18:40 +0530

    Cs-Event Folder change

--- Cs-Event/js/countdown-custom.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-        jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2018, 2, 16, 8)}); // year, month, date, hour
-        });
-});		
-
